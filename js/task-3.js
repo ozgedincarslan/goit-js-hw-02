a73edde8fb54a4576ba021b3c71405651e210069
ozge_d <yureksoken@hotmail.com>
2025-02-23 21:42:42 +0300

    update task3

--- js/task-3.js
+++ js/task-3.js
@@ -1,8 +1,6 @@
 function checkForSpam(message) {
   const lowerCaseMessage = message.toLowerCase();
-  if (lowerCaseMessage.includes('spam')) {
-    return true;
-  } else if (lowerCaseMessage.includes('sale')) {
+  if (lowerCaseMessage.includes('spam') || lowerCaseMessage.includes('sale')) {
     return true;
   } else {
     return false;
